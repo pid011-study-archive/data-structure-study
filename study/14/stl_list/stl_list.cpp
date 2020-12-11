ac7abef34361014a00c5ea7089a80a2e591d3975
pid011 <pid011@kakao.com>
2020-12-11 15:04:48 +0900

    feat: add week 14 project

---
+++ study/14/stl_list/stl_list.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <list>
+
+class Item {
+    int _id;
+    int _money;
+
+public:
+    Item(int id, int money) : _id(id), _money(money) { }
+
+    int id() const {
+        return _id;
+    }
+
+    int money() const {
+        return _money;
+    }
+};
+
+int main() {
+    using namespace std;
+
+    list<Item> items;
+    items.push_front(Item(1, 2000));
+    items.push_front(Item(2, 1000));
+    items.push_back(Item(3, 3000));
+    items.push_back(Item(4, 4500));
+
+    list<Item>::iterator iter_end = items.end();
+    for (list<Item>::iterator iter_pos = items.begin(); iter_pos != iter_end; iter_pos++) {
+        cout << "아이템 코드 : " << iter_pos->id() << endl;
+    }
+}
